711401db259050f31b428c74750bc21f6d65ccc9
gzzchh <xjdzch@126.com>
2020-09-19 23:18:18 +0800

    大体框架做出来了

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	title: "声呐快起床",
+	description: "不然我就不干了",
+	dest: "./dist",
+	port: "7777",
+	head: [["link", { rel: "icon", href: "/logo.gif" }]],
+	markdown: {
+		lineNumbers: true,
+	},
+	themeConfig: {
+		nav: require("./nav"),
+		sidebar: require("./sidebar"),
+		sidebarDepth: 2,
+		lastUpdated: "Last Updated",
+		searchMaxSuggestoins: 10,
+		serviceWorker: {
+			updatePopup: {
+				message: "New content is available.",
+				buttonText: "Refresh",
+			},
+		},
+		editLinks: true,
+		editLinkText: "在 GitHub 上编辑此页 ！",
+	},
+};
---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,14 @@
+module.exports = [
+	{
+		text: "首页",
+		link: "/",
+	},
+	{
+		text: "Java版",
+		link: "/java-edition/",
+	},
+	{
+		text: "基岩版",
+		link: "/bedrock-edition/",
+	},
+];
